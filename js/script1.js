c0fdd88b7deebfc928342df86bf2e1f5312956ec
Hieu Dep Trai <checkerpss@gmail.com>
2023-01-28 00:34:13 +0700

    Upload next

--- js/script1.js
+++ js/script1.js
@@ -89,3 +89,70 @@
 		sliderCurrent.classList.add("mobile-show");
 	});
 })();
+(() => {
+	let arrowElement = document.querySelector(".arrow");
+	let svgCircleElement = arrowElement.querySelector("svg #circle-2");
+	const svgMath = 131.88;
+	window.addEventListener("scroll", () => {
+		let htmlScrollTop = document.documentElement.scrollTop;
+		if(htmlScrollTop >= 23) {
+			arrowElement.classList.add("arrow-active");
+			svgCircleElement.style.strokeDashoffset = `${svgMath - ((htmlScrollTop / 4416) * 131.88)}`;
+		}else {
+			arrowElement.classList.remove("arrow-active");
+		}
+	});
+})();
+function checkInclude(searchBodyLink, searchInputValue) {
+	return searchBodyLink.includes(searchInputValue);
+}
+(() => {
+	var cartMenuFont = document.querySelector(".cart__menu-font");
+	var searchElement = document.querySelector(".search");
+	var searchBodyElement = searchElement.querySelector(".search__body");
+	var searchInput = searchBodyElement.querySelector("input");
+	var searchBodyList = searchBodyElement.querySelector(".search__body-list");
+	var searchBodyListItem = searchBodyList.querySelectorAll(".search__body-list-item");
+	searchInput.addEventListener("input", (e) => {
+			searchBodyListItem.forEach(v => {
+			var searchBodyLink = v.querySelector(".search__body-list-link");
+			var searchBodyLinkValue = searchBodyLink.textContent;
+			searchBodyLinkValue = searchBodyLinkValue.replace(/\snew$|\shot$/i, "");
+			if(!checkInclude(searchBodyLinkValue.toLowerCase(), e.target.value.toLowerCase())) {
+				v.hidden = true;
+			}else {
+				v.hidden = false;
+			}
+			});
+	});	
+	cartMenuFont.addEventListener("click", () => {
+		searchElement.classList.toggle("search-zindex");
+		searchBodyElement.classList.toggle("search__body-active");
+		setTimeout(() => {
+			searchElement.classList.toggle("search-active");
+		}, 1000)
+	});
+	var buttonDevide = searchBodyList.querySelectorAll(".search__body-list-devide-button");
+	var productItem = searchBodyList.querySelectorAll(".search__body-list-product-item");
+	buttonDevide.forEach(v => {
+		v.addEventListener("click", () => {
+			var productWrap = v.parentElement.nextElementSibling;
+			if(productWrap.classList.contains("effect-max-height")) {
+				productWrap.classList.remove("effect-max-height");
+				var productBox = productWrap.querySelectorAll(".search__body-list-product-box");
+				productBox.forEach(e => {
+					e.classList.remove("effect-max-height");
+				})
+			}else {
+				productWrap.classList.add("effect-max-height");
+			}
+		});
+	})
+	productItem.forEach(v => {
+		v.addEventListener("click", (e) => {
+			if(e.target.tagName != "BUTTON" && e.target.tagName != "I") return;
+			var productBox = e.currentTarget.querySelector(".search__body-list-product-box");
+			productBox.classList.toggle("effect-max-height");
+		});
+	})
+})();
